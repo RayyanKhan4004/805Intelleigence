b9d25f1f77229b811209a254ab9b4eca95ca98f9
library-api <mughalmosin82@gmail.com>
2026-01-05 18:08:35 +0500

    feat(button) - Added button varients

--- src/components/theme/Button.tsx
+++ src/components/theme/Button.tsx
@@ -3,9 +3,18 @@ import { type ButtonProps } from "@/types";
 import {ButtonEnums} from "@/components/enums/index"
 function Button({ variant, children }: ButtonProps) {
   function GetVariantStyles(type: string) {
-    switch (type) {
-      case ButtonEnums.primary:
-        return "w-full";
+     switch (type) {
+      case ButtonEnums.PRIMARY:
+        return "w-full bg-blue-600 text-white";
+
+      case ButtonEnums.SECONDARY:
+        return "w-full bg-gray-600 text-white";
+
+      case ButtonEnums.OUTLINE:
+        return "w-full border border-blue-600 text-blue-600";
+
+      default:
+        return "";
     }
   }
   return <div className={`${GetVariantStyles(variant)}`}>{children}</div>;
