48c288089587dbcdbdfa67a0d9d27f3b61da54a1
library-api <mughalmosin82@gmail.com>
2026-01-09 09:54:32 +0500

    added typo

--- src/components/theme/Typography.tsx
+++ src/components/theme/Typography.tsx
@@ -1,1 +1,12 @@
-import react from "react";
+import React from "react";
+import { type TypographyProps } from "@/types";
+import {TypographyEnums} from "@/components/enums/index"
+import {TypographyVariants} from "@/components/enums/index"
+function Typography({ variant, children }: TypographyProps) {
+    function getsize(variant:string){
+ const ennum = TypographyEnums?.variant || "";
+ return TypographyVariants.ennum || "";
+    }
+    return <p className={`test-[${getsize(variant)}px]`}></p>
+}
+ 
