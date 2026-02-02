5323fb6b0018276e36d40a4064d12dfdbf5a82ff
Abdulwahab340 <ranaabdulwahab793@gmail.com>
2026-02-02 17:52:04 +0500

    redirect_reports

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,10 +1,13 @@
+import Navbar from '@/feature/navBar/components/Navbar'
 import Header from '@/feature/reports/components/header'
 import React from 'react'
 
 function page() {
   return (
     <div>
+      <Navbar></Navbar>
      <Header></Header>
+     
       
     </div>
   )
