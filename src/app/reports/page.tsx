53dd980740b0d1dcd88c3d4c0e0314502548e2f5
Abdulwahab340 <ranaabdulwahab793@gmail.com>
2026-02-03 16:13:08 +0500

    feat: add dialog component and integrate with custom popup; update dependencies

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,12 +1,25 @@
+// 'use client'
 import Navbar from '@/feature/navBar/components/Navbar'
 import Header from '@/feature/reports/components/header'
-import React from 'react'
+// import React from 'react'
+// import CustomPopup from '@/components/theme/CustomPopUp'
 
 function page() {
+  // const [isOpen, setIsOpen] = React.useState(true)
   return (
     <div>
-     <Navbar></Navbar>
-     <Header></Header>
+      <Navbar></Navbar>
+      <Header></Header>
+      {/* <CustomPopup
+        title="ertyh"
+        Description="ertyh"
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
+        kjhgfdsa
+      </CustomPopup> */}
+
+      {/* <div className="h-4" onClick={() => setIsOpen(true)}>hjhbnknkn</div> */}
     </div>
   )
 }
--- src/components/types/index.ts
+++ src/components/types/index.ts
@@ -3,3 +3,4 @@ export type { props as TypographyProps } from "./Typography";
 export type { props as InputProps } from "./Input";
 export type { props as cardProps  } from "./CardType";
 export type { props as ReportRow  } from "./ReportType";
+export type { props as DialogProps  } from "./DialogType";
