8034ac24c1da44294fc50f284fc23d66396317bb
Abdulwahab340 <ranaabdulwahab793@gmail.com>
2026-02-24 13:03:43 +0500

    fixed side bar

--- src/features/navBar/components/SideBar.tsx
+++ src/features/navBar/components/SideBar.tsx
@@ -1,15 +1,27 @@
+import { useEffect } from 'react'
 import NavTabs from './NavTabs'
 function SideBar({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add('max-md:overflow-hidden')
+    } else {
+      document.body.classList.remove('max-md:overflow-hidden')
+    }
+    return () => {
+      document.body.classList.remove('max-md:overflow-hidden')
+    }
+  }, [isOpen])
   return (
     <>
-      {/* Backdrop (outside click) */}
-      {isOpen && (
-        <div
-          onClick={onClose}
-          className="fixed inset-0 bg-black/20 z-20 md:hidden duration-500 transition-all delay-200"
-        />
-      )}
-
+      <div
+        onClick={onClose}
+        className={`
+    fixed inset-0 z-20 md:hidden
+    bg-black/20
+    transition-opacity duration-700 
+    ${isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}
+  `}
+      />
       <div
         onClick={e => e.stopPropagation()}
         className={` fixed top-[60px] bg-primary md:hidden h-full z-30 duration-700 transition-all ${isOpen ? ' w-[60vw]' : ' !w-0 overflow-hidden'}`}
