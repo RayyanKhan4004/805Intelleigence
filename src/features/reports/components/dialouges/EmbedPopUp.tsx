99508b4ee1fa2d5183b33322a3b7079f56762162
Abdulwahab340 <ranaabdulwahab793@gmail.com>
2026-03-02 15:19:19 +0500

    Embed popup button state change

--- src/features/reports/components/dialouges/EmbedPopUp.tsx
+++ src/features/reports/components/dialouges/EmbedPopUp.tsx
@@ -12,17 +12,36 @@ import {
 import { Checkbox } from '@/components/UI/checkbox'
 import { Label } from '@/components/UI/label'
 import CustomAreaChart from '@/components/theme/AreaChart'
-
+const tabs: Array<{ label: string; value: 'Image' | 'Lead Form' | 'Search Box'; title: string }> = [
+  {
+    label: 'Image',
+    value: 'Image',
+    title: 'Los Angeles County, CA',
+  },
+  {
+    label: 'Lead Form',
+    value: 'Lead Form',
+    title: 'Build Lead Form',
+  },
+  {
+    label: 'Search Box',
+    value: 'Search Box',
+    title: 'Search Box ',
+  },
+]
 function EmbedPopUp() {
   const [isOpen, setIsOpen] = React.useState(true)
+  const [selectedTab, setSelectedTab] = React.useState(tabs[0])
+
   return (
     <div>
       <CustomPopup
         title={
           <div className="flex justify-between items-center font-semibold text-[25px] text-app-primary text-nowrap">
-            Los Angeles County, CA
-            <div className="flex gap-[15px]">
-              <Button className="font-medium text-[20px] text-white" variant="rounded">
+            {/* Los Angeles County, CA */}
+            {selectedTab.title}
+            {/* <div className="flex gap-[15px]">
+              <Button  className="font-medium text-[20px] text-white" variant="rounded">
                 Image
               </Button>
               <Button className="font-medium text-[20px] text-app-primary" variant="roundedOutline">
@@ -31,6 +50,20 @@ function EmbedPopUp() {
               <Button className="font-medium text-[20px] text-app-primary" variant="roundedOutline">
                 Search Box
               </Button>
+            </div> */}
+            <div className="flex gap-[15px] ">
+              {tabs.map(curr => {
+                return (
+                  <Button
+                    className=" font-medium text-[20px]  "
+                    onClick={() => setSelectedTab(curr)}
+                    key={curr.value}
+                    variant={selectedTab.value === curr.value ? 'rounded' : 'roundedOutline'}
+                  >
+                    {curr.label}
+                  </Button>
+                )
+              })}
             </div>
           </div>
         }
@@ -94,7 +127,7 @@ function EmbedPopUp() {
               </div>
             </div>
           </div>
-          {/* <div className="flex flex-col"> */}
+
           <div className="border border-app-stroke rounded-[16px] text-[14px] text-app-greyText p-[20px] ">
             {
               '  <a href="https://altos.re/r/7aff8dda-9b08-4c7c-9ee3-3461426c0b6b?data=price_median" target="_blank"><img width="600px" src="https://altos.'
@@ -105,7 +138,6 @@ function EmbedPopUp() {
             {
               ' <a href="https://altos.re/r/7aff8dda-9b08-4c7c-9ee3-3461426c0b6b?data=price_median" target="_blank"><img width="600px" src="https://altos. '
             }
-            {/* </div> */}
           </div>
           <div className="font-bold text-[20px] text-app-primary">Preview</div>
 
