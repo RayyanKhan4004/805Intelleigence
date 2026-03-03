5e550eb88045918b913401819a3f37e914755e11
Abdulwahab340 <ranaabdulwahab793@gmail.com>
2026-03-03 13:09:23 +0500

    Embed popup fix

--- src/features/reports/components/dialouges/EmbedPopUp/tabs/LeadForm.tsx
+++ src/features/reports/components/dialouges/EmbedPopUp/tabs/LeadForm.tsx
@@ -14,8 +14,10 @@ import { RadioGroup, RadioGroupItem } from '@/components/UI/radio-group'
 
 function LeadForm() {
   return (
-    <div>
-      <div>Select Campaign</div>
+    <div className='flex flex-col gap-[30px]'>
+      <div className='flex flex-col gap-[20px]'>
+        <div className='flex flex-col gap-[20px]'>
+      <div className='text-[20px] text-app-primary'>Select Campaign</div>
       <div>
         <Select>
           <SelectTrigger className="w-full h-[60px]">
@@ -23,7 +25,9 @@ function LeadForm() {
           </SelectTrigger>
         </Select>
       </div>
-      <div>Select Reports</div>
+      </div>
+         <div className='flex flex-col gap-[20px]' >
+      <div className='text-[20px] text-app-primary'>Select Reports</div>
       <div>
         <Select>
           <SelectTrigger className="w-full h-[60px]">
@@ -31,9 +35,12 @@ function LeadForm() {
           </SelectTrigger>
         </Select>
       </div>
+      </div>
+
+       <div className='flex flex-col gap-[20px]'>
       <div className="flex items-center gap-[12px] ">
-        <Checkbox className="border border-app-greyText" />
-        <Label className="text-app-greyText text-[14px]">Add Custom Branding</Label>
+        <Checkbox className="border border-app-greyText w-[25px] h-[25px]" />
+        <Label className='text-[20px] text-app-primary'>Add Custom Branding</Label>
       </div>
 
       <div className="flex items-center gap-[15px]">
@@ -43,48 +50,57 @@ function LeadForm() {
         </div>
         <Button>Copy Link</Button>
       </div>
-
-      <div className="flex items-center justify-between">
+      </div>
+   </div>
+       <div className=" w-full  rounded-[20px] shadow-shadow p-5">
+      <div className="flex items-center justify-between ">
         <div className="font-medium text-[20px] text-app-primary">Embed Code Options</div>
         <Button variant="outline">Copy Link</Button>
       </div>
-      <div className="border-b border-app-stroke pb-2.5"></div>
+      <div className="border-b border-app-stroke pb-2.5 mb-[20px]"></div>
+     
+
+     <div className='flex flex-col gap-[25px]'>
+      <div className='flex flex-col gap-[20px]'>
       <div className="text-[18px] text-app-primary">Select Embed Size</div>
 
       <div>
         <RadioGroup className="flex items-center gap-[57px]">
-          <div>
+          <div className="flex items-center gap-[15px]">
             <RadioGroupItem value="small" id="small" />
-            <Label>Small</Label>
+            <Label className='text-[14px] text-app-greyText'>Small</Label>
           </div>
 
-          <div>
+          <div className="flex items-center gap-[15px]">
             <RadioGroupItem value="medium" id="medium" />
-            <Label>Medium</Label>
+            <Label className='text-[14px] text-app-greyText'>Medium</Label>
           </div>
 
-          <div>
-            <RadioGroupItem value="large" id="large" />
-            <Label>Large</Label>
+          <div className="flex items-center gap-[15px]">
+            <RadioGroupItem  value="large" id="large" />
+            <Label className='text-[14px] text-app-greyText'>Large</Label>
           </div>
 
-          <div>
+          <div className="flex items-center gap-[15px]">
             <RadioGroupItem value="xl" id="xl" />
-            <Label>XL</Label>
+            <Label className='text-[14px] text-app-greyText'>XL</Label>
           </div>
         </RadioGroup>
       </div>
+      </div>
 
-      <div>
+      <div className='flex flex-col gap-[15px]'>
         <div className="text-[18px] text-app-primary">Generated Embed Code</div>
-        <div className="border border-app-stroke text-[14px] text-app-greyText pt-[21px] pb-[58px] pl-[20px]">
+        <div className="border border-app-stroke rounded-[16px] text-[14px] text-app-greyText pt-[21px] pb-[58px] pl-[20px]">
           {
             '<iframe src="https://altos.re/keep-informed?a=5628&embed=1&s=md" style="border:0;display:block;width:480px;max-width:100%;height:640px" scrolling="auto" loading="lazy"></iframe>'
           }
         </div>
+        </div>
+        </div>
       </div>
 
-      <Button variant="outline" className="font-medium text-app-primary text-[16px]">
+      <Button variant="outline" className="font-medium text-app-primary text-[16px] justify-start w-fit">
         Cancel
       </Button>
     </div>
--- src/features/reports/components/dialouges/EmbedPopUp/tabs/SearchBox.tsx
+++ src/features/reports/components/dialouges/EmbedPopUp/tabs/SearchBox.tsx
@@ -7,7 +7,8 @@ function SearchBox() {
      const [isOpen, setIsOpen] = React.useState(true)
   return (
     <div>
-      <div>Link</div>
+      <div className='flex flex-col gap-[15px] mb-[20px]'>
+      <div className='text-[20px] text-app-primary'>Link</div>
        <div>
             <Select>
               <SelectTrigger className="w-full h-[60px]">
@@ -15,13 +16,16 @@ function SearchBox() {
               </SelectTrigger>
             </Select>
             </div>
-            <div className='flex justify-end'>
+            </div>
+            <div className='flex justify-end gap-[10px]'>
            <Button variant="outline" className='font-medium text-[16px] text-app-primary border border-app-primary'>Goto</Button>
            <Button className='text-white text-[16px] font-medium'>Copy Link</Button>
             </div>
+            <div className='flex flex-col gap-[15px] mb-[40px] mt-[20px]'>
             <div className='text-[18px] text-app-primary '>Show Embed Code</div>
-            <div className='text-[14px] text-app-greyText pt-[21px] pb-[58px] pl-[20px] pr-[63px]'>
+            <div className='text-[14px] text-app-greyText pt-[21px] pb-[58px] pl-[20px] pr-[63px] border border-app-stroke rounded-[16px]'>
                 {'<iframe src="https://altos.re/keep-informed?a=5628&embed=1&s=md" style="border:0;display:block;width:480px;max-width:100%;height:640px" scrolling="auto" loading="lazy"></iframe>'}
+            </div>
             </div>
             <Button variant='outline' className='font-medium text-app-primary text-[16px]'>Close</Button>
     </div>
